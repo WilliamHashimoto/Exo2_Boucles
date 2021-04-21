f5d79aa9aa71038817dc80de951b672f45638491
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-04-21 15:09:47 +0200

    fini

---
+++ main.js
@@ -0,0 +1,11 @@
+let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+
+eleves.forEach(element =>{
+    console.log("bonjour " + element);
+})
+
+let array = ["pomme", "poire", "citron", "orange", "fraise"];
+
+array.forEach(element =>{
+    console.log(element);
+})
